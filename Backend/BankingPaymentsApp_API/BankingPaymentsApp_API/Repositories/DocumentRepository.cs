46730188f1da584ded14df05bb942347dd542076
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 17:05:00 +0530

    created Document Repository

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Repositories/DocumentRepository.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Repositories/DocumentRepository.cs
@@ -1,6 +1,54 @@
-﻿namespace BankingPaymentsApp_API.Repositories
+﻿using BankingPaymentsApp_API.Data;
+using BankingPaymentsApp_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingPaymentsApp_API.Repositories
 {
     public class DocumentRepository
     {
+        private readonly BankingPaymentsDBContext _dbContext;
+        public DocumentRepository(BankingPaymentsDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+        public IEnumerable<Document> GetAll()
+        {
+            return _dbContext.Documents.ToList();
+        }
+
+        public Document Add(Document document)
+        {
+            _dbContext.Documents.Add(document);
+            _dbContext.SaveChanges();
+            return document;
+        }
+
+        public Document? getById(int id)
+        {
+            return _dbContext.Documents.Include(d=>d.ProofType).FirstOrDefault(d => d.DocumentId.Equals(id));
+        }
+
+        public Document? Update(Document document)
+        {
+            Document? existingDocument = getById(document.DocumentId);
+
+            if (existingDocument == null)
+                return null;
+
+            existingDocument.DocumentName = document.DocumentName;
+            existingDocument.DocumentURL = document.DocumentURL;
+            existingDocument.ProofTypeId = document.ProofTypeId;
+
+            _dbContext.SaveChanges();
+            return existingDocument;
+        }
+
+        public void DeleteById(int id)
+        {
+            Document? exisitngDocument = getById(id);
+            if (exisitngDocument == null) return;
+            _dbContext.Documents.Remove(exisitngDocument);
+        }
     }
 }
