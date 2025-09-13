5bdef9a08d4e0284758d3ec98f87e86ed8213b74
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 12:05:59 +0530

    created Payment Status Enum Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/PaymentStatus.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/PaymentStatus.cs
@@ -1,6 +1,13 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingPaymentsApp_API.Models
 {
+    public enum PayStatus { APPROVED, DECLINED, PENDING }
     public class PaymentStatus
     {
+        [Key]
+        public int StatusId { get; set; }
+        [Required(ErrorMessage = "Status is Required!")]
+        public PayStatus Status { get; set; }
     }
 }
