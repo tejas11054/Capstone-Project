0ff3bd2b82d215790f9a5ea029b7b74e6d31c5cc
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 12:19:33 +0530

    created User Role Enum Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/UserRole.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/UserRole.cs
@@ -1,6 +1,13 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingPaymentsApp_API.Models
 {
+    public enum Role { ADMIN, BANK_USER, CLIENT_USER }
     public class UserRole
     {
+        [Key]
+        public int RoleId { get; set; }
+        [Required(ErrorMessage = "Role is Required!")]
+        public Role Role { get; set; }
     }
 }
