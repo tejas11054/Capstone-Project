23e839df33332f1a7fc6cd5b7f3158573e59ca9c
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 14:57:04 +0530

    created Transaction and Beneficiary Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Transaction.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Transaction.cs
@@ -1,6 +1,25 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingPaymentsApp_API.Models
 {
     public class Transaction
     {
+        [Key]
+        public int TransactionId {  get; set; }
+        [ForeignKey("Account")]
+        public int AccountId {  get; set; }
+        public virtual Account? Account { get; set; }
+        [ForeignKey("Payment")]
+        public int PayementId {  get; set; }
+        public virtual Payment? Payment { get; set; }
+        [Required(ErrorMessage ="Transaction Type is Required!")]
+        public TxnType TransactionType { get; set; }
+        [Required(ErrorMessage = "Transaction Amount is Required!")]
+        [DataType(DataType.Currency)]
+        public int Amount {  get; set; }
+        [Required(ErrorMessage = "Transaction Date is Required!")]
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
