5f4d5ea6963dcfea7b45031b8ae89988bb917403
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 12:13:37 +0530

    created Document Proof Type Enum Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/ProofType.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/ProofType.cs
@@ -1,6 +1,20 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingPaymentsApp_API.Models
 {
+    public enum DocProofType
+    {
+        IDENTITY_PROOF,
+        ADDRESS_PROOF,
+        DATE_OF_BIRTH_PROOF,
+        PHOTOGRAPH,
+        PAN_CARD
+    }
     public class ProofType
     {
+        [Key]
+        public int ProofId { get; set; }
+        [Required(ErrorMessage = "Type is Required!")]
+        public AccStatus Type { get; set; }
     }
 }
