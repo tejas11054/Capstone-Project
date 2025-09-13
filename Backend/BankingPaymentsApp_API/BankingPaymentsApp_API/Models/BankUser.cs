8dcb2f60b76dac59f4df3393f47251f5d3e014eb
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 13:54:31 +0530

    created Bank User Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/BankUser.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/BankUser.cs
@@ -1,6 +1,12 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingPaymentsApp_API.Models
 {
-    public class BankUser
+    public class BankUser : User
     {
+        [Required(ErrorMessage = "Refferal code is Required!")]
+        public string RefferalCode { get; set; }
+        [Required(ErrorMessage = "branch is Required!")]
+        public string Branch {  get; set; }
     }
 }
