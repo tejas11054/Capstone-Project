861ca995ddbfb9d0e2de4a2918807a855758d284
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 16:55:47 +0530

    created Payment Repository

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Repositories/PaymentRepository.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Repositories/PaymentRepository.cs
@@ -1,6 +1,55 @@
-﻿namespace BankingPaymentsApp_API.Repositories
+﻿using BankingPaymentsApp_API.Data;
+using BankingPaymentsApp_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingPaymentsApp_API.Repositories
 {
     public class PaymentRepository
     {
+        private readonly BankingPaymentsDBContext _dbContext;
+        public PaymentRepository(BankingPaymentsDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+        public IEnumerable<Payment> GetAll()
+        {
+            return _dbContext.Payments.ToList();
+        }
+
+        public Payment Add(Payment payment)
+        {
+            _dbContext.Payments.Add(payment);
+            _dbContext.SaveChanges();
+            return payment;
+        }
+
+        public Payment? getById(int id)
+        {
+            return _dbContext.Payments.Include(p=>p.PayerAccount).Include(p=>p.PayeeAccount).Include(p=>p.PaymentStatus).FirstOrDefault(p => p.PaymentId.Equals(id));
+        }
+
+        public Payment? Update(Payment payment)
+        {
+            Payment? existingPayment = getById(payment.PaymentId);
+
+            if (existingPayment == null)
+                return null;
+
+            existingPayment.PayerAccountId = payment.PayerAccountId;
+            existingPayment.PayeeAccountId = payment.PayeeAccountId;
+            existingPayment.Amount = payment.Amount;
+            existingPayment.PaymentStatusId = payment.PaymentStatusId;
+
+            _dbContext.SaveChanges();
+            return existingPayment;
+        }
+
+        public void DeleteById(int id)
+        {
+            Payment? exisitngPayment = getById(id);
+            if (exisitngPayment == null) return;
+            _dbContext.Payments.Remove(exisitngPayment);
+        }
     }
 }
