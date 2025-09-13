fb6ba2d1b9d3d1eeecf5eee2bb9e5241f689b421
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 14:37:56 +0530

    created Payment model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Payment.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Payment.cs
@@ -1,6 +1,28 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingPaymentsApp_API.Models
 {
     public class Payment
     {
+        [Key]
+        public int PaymentId {  get; set; }
+        [ForeignKey("PayerAccount")]
+        public int PayerAccountId {  get; set; }
+        public virtual Account? PayerAccount {  get; set; }
+        [ForeignKey("PayeeAccount")]
+        public int PayeeAccountId {  get; set; }
+        public virtual Account? PayeeAccount { get; set; }
+        [Required(ErrorMessage = "Amount is Required!")]
+        [DataType(DataType.Currency)]
+        public double Amount {  get; set; }
+        [Required(ErrorMessage = "CreatedAt is Required!")]
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public PayStatus PaymentStatus { get; set; } = PayStatus.PENDING;
+        [Required(ErrorMessage = "ActionAt is Required!")]
+        [DataType(DataType.DateTime)]
+        public DateTime ActionAt { get; set; } = DateTime.Now;
+        public List<int>? TransactionIds { get; set; } = new List<int>();
     }
 }
