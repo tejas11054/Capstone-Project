3a17d5c7c0872a21e1ff1f368567f8df2825d542
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 17:01:12 +0530

    created Employee Repository

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Repositories/EmployeeRepository.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Repositories/EmployeeRepository.cs
@@ -1,6 +1,56 @@
-﻿namespace BankingPaymentsApp_API.Repositories
+﻿using BankingPaymentsApp_API.Data;
+using BankingPaymentsApp_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingPaymentsApp_API.Repositories
 {
     public class EmployeeRepository
     {
+        private readonly BankingPaymentsDBContext _dbContext;
+        public EmployeeRepository(BankingPaymentsDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _dbContext.Employees.ToList();
+        }
+
+        public Employee Add(Employee employee)
+        {
+            _dbContext.Employees.Add(employee);
+            _dbContext.SaveChanges();
+            return employee;
+        }
+
+        public Employee? getById(int id)
+        {
+            return _dbContext.Employees.Include(e=>e.ClientUser).FirstOrDefault(e => e.EmployeeId.Equals(id));
+        }
+
+        public Employee? Update(Employee employee)
+        {
+            Employee? existingEmployee = getById(employee.EmployeeId);
+
+            if (existingEmployee == null)
+                return null;
+
+            existingEmployee.EmployeeName = employee.EmployeeName;
+            existingEmployee.ClientId = employee.ClientId;
+            existingEmployee.AccountNumber = employee.AccountNumber;
+            existingEmployee.BankName = employee.BankName;
+            existingEmployee.IFSC=employee.IFSC;
+
+            _dbContext.SaveChanges();
+            return existingEmployee;
+        }
+
+        public void DeleteById(int id)
+        {
+            Employee? exisitngEmployee = getById(id);
+            if (exisitngEmployee == null) return;
+            _dbContext.Employees.Remove(exisitngEmployee);
+        }
     }
 }
