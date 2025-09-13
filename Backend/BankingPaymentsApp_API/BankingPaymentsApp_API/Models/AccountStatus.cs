fdfd53690cf1c520dcdbb6898774dd3c80103579
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 12:01:20 +0530

    created Account Status Enum Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/AccountStatus.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/AccountStatus.cs
@@ -1,6 +1,13 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingPaymentsApp_API.Models
 {
+    public enum AccStatus { ACTIVE, INACTIVE, CLOSED}
     public class AccountStatus
     {
+        [Key]
+        public int StatusId {  get; set; }
+        [Required(ErrorMessage = "Status is Required!")]
+        public AccStatus Status { get; set; }
     }
 }
