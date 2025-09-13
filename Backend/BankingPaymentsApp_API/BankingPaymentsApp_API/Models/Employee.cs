c06a5ee2292e37dd8d478c1b7a67ae78eb077255
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 14:27:00 +0530

    created Employee model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Employee.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Employee.cs
@@ -1,6 +1,22 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingPaymentsApp_API.Models
 {
     public class Employee
     {
+        [Key]
+        public int EmployeeId { get; set; }
+        [ForeignKey("ClientUser")]
+        public int ClientId {  get; set; }
+        public virtual ClientUser? ClientUser { get; set; }
+        [Required(ErrorMessage = "Employee Name is Required!")]
+        public string EmployeeName {  get; set; }
+        [Required(ErrorMessage = "Account Number is Required!")]
+        public string AccountNumber {  get; set; }
+        [Required(ErrorMessage = "Bank Name is Required!")]
+        public string BankName {  get; set; }
+        [Required(ErrorMessage = "IFSC Code is Required!")]
+        public string IFSC {  get; set; }
     }
 }
