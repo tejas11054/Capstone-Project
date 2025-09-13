61cb82dac4d298f2cfe0229918fb6154745fb3ee
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 14:58:36 +0530

    created Transaction and Beneficiary Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Beneficiary.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Beneficiary.cs
@@ -1,6 +1,21 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingPaymentsApp_API.Models
 {
     public class Beneficiary
     {
+        [Key]
+        public int BeneficiaryId {  get; set; }
+        [Required(ErrorMessage = "clientId is Required!")]
+        public int ClientId {  get; set; }
+        [Required(ErrorMessage = "Beneficiary name is Required!")]
+        public string BeneficiaryName { get; set; }
+        [Required(ErrorMessage = "AccountNumber is Required!")]
+        public string AccountNumber {  get; set; }
+        [Required(ErrorMessage = "Bank name is Required!")]
+        public string BankName {  get; set; }
+        [Required(ErrorMessage = "IFSC Code is Required!")]
+        public string IFSC {  get; set; }
+
     }
 }
