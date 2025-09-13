7051014bfd17a0b93acfd16007d782c6c8366eb9
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 12:38:49 +0530

    created Account Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Account.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/Account.cs
@@ -1,6 +1,28 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingPaymentsApp_API.Models
 {
     public class Account
     {
+        [Key]
+        public int AccountId {  get; set; }
+        [Required(ErrorMessage = "Account Number is Required!")]
+        [RegularExpression(@"BPS[0-9]{9}",ErrorMessage ="Account Number is Not Valid")]
+        public string AccountNumber {  get; set; }
+        [ForeignKey("ClientUser")]
+        public int? ClientId {  get; set; }
+        public virtual ClientUser? ClientUser { get; set; }
+        [Required(ErrorMessage = "Balance in Required!")]
+        [DataType(DataType.Currency)]
+        public double Balance { get; set; } = 0;
+        [Required(ErrorMessage = "Account Type is Required!")]
+        public AccType AccountType { get; set; } = AccType.CURRENT;
+        [Required(ErrorMessage = "Account Status is Required!")]
+        public AccStatus AccountStatus { get; set; } = AccStatus.ACTIVE;
+        [Required(ErrorMessage = "Account Creation Date is Required!")]
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt {  get; set; } = DateTime.Now.Date;
+        public virtual List<int>? TransactionIds { get; set; }
     }
 }
