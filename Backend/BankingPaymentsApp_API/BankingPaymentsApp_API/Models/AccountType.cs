b153375831f56b61a5e7512361dba8da5edfc4bb
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-13 12:03:23 +0530

    created Account Type Enum Model

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/AccountType.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/AccountType.cs
@@ -1,6 +1,13 @@
-﻿namespace BankingPaymentsApp_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingPaymentsApp_API.Models
 {
+    public enum AccType { SAVINGS, CURRENT, SALARY }
     public class AccountType
     {
+        [Key]
+        public int TypeId { get; set; }
+        [Required(ErrorMessage = "Type is Required!")]
+        public AccType Type { get; set; }
     }
 }
