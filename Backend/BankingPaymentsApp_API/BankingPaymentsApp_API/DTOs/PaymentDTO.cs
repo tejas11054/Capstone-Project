687c3ef26b97df59a351782c778f186b4a0025c2
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-14 18:48:30 +0530

    added Beneficiary Employee Payment and Transaction DTO

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/DTOs/PaymentDTO.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/DTOs/PaymentDTO.cs
@@ -1,6 +1,21 @@
-﻿namespace BankingPaymentsApp_API.DTOs
+﻿using BankingPaymentsApp_API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingPaymentsApp_API.DTOs
 {
     public class PaymentDTO
     {
+        public int PayerAccountId { get; set; }
+        public int PayeeAccountId { get; set; }
+        [Required(ErrorMessage = "Amount is Required!")]
+        [DataType(DataType.Currency)]
+        public double Amount { get; set; }
+        public int PaymentStatusId { get; set; } = 3;
+        public virtual PaymentStatus? PaymentStatus { get; set; }
+        [Required(ErrorMessage = "ActionAt is Required!")]
+        [DataType(DataType.DateTime)]
+        public DateTime ActionAt { get; set; } = DateTime.Now;
+        public List<int>? TransactionIds { get; set; } = new List<int>();
     }
 }
