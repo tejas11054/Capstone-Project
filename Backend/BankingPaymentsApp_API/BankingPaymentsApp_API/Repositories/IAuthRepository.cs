28c013e716811960b28558d710729f03de484aac
tejas11054 <tejramane11@gmail.com>
2025-09-16 14:26:58 +0530

    Auth repository added

---
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Repositories/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿using BankingPaymentsApp_API.DTOs;
+
+namespace BankingPaymentsApp_API.Repositories
+{
+    public interface IAuthRepository
+    {
+        public LoginResponseDTO Login(LoginDTO usr);
+    }
+}
