1fb0b4be2e2b1ea8f3eb4fb5c87326b54069e417
tejas11054 <tejramane11@gmail.com>
2025-09-16 14:16:21 +0530

    login DTO changes

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/DTOs/LoginDTO.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/DTOs/LoginDTO.cs
@@ -1,8 +1,14 @@
-﻿namespace BankingPaymentsApp_API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingPaymentsApp_API.DTOs
 {
     public class LoginDTO
     {
+        [Required(ErrorMessage = "UserName is Required!")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is Required!")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
