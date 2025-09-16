48a4ccc7a63853b9a128b1c928954edc1d99d4a6
tejas11054 <tejramane11@gmail.com>
2025-09-16 14:39:12 +0530

    Auth controller added

---
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using BankingPaymentsApp_API.DTOs;
+using BankingPaymentsApp_API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingPaymentsApp_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult PostLogin([FromBody] LoginDTO usr)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = _authService.Login(usr);
+                if (response.IsSuccess)
+                {
+                    return Ok(response.Token);
+                }
+                return Unauthorized();
+            }
+            return BadRequest();
+        }
+    }
+}
