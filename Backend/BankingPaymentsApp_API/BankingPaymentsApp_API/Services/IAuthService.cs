e0c11aacbceb9508cb6ac2f7a025e1f68ea305e8
tejas11054 <tejramane11@gmail.com>
2025-09-16 14:34:40 +0530

    Auth service added

---
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using BankingPaymentsApp_API.DTOs;
+
+namespace BankingPaymentsApp_API.Services
+{
+    public interface IAuthService
+    {
+        public LoginResponseDTO Login(LoginDTO usr);
+    }
+}
