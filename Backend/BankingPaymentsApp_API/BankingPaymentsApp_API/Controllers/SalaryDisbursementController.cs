93309d239efb7a802497b4c4730717b9558e0c58
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-17 23:05:52 +0530

    Added Salary related Services

---
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Controllers/SalaryDisbursementController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingPaymentsApp_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalaryDisbursementController : ControllerBase
+    {
+    }
+}
---
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Services/SalaryDisbursementDetailsService.cs
@@ -0,0 +1,37 @@
+﻿using BankingPaymentsApp_API.Models;
+using BankingPaymentsApp_API.Repositories;
+
+namespace BankingPaymentsApp_API.Services
+{
+    public class SalaryDisbursementDetailsService : ISalaryDisbursementDetailsService
+    {
+        private readonly ISalaryDisbursementDetailsRepository _repository;
+
+        public SalaryDisbursementDetailsService(ISalaryDisbursementDetailsRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IEnumerable<SalaryDisbursementDetails>> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+
+        public async Task<SalaryDisbursementDetails> Add(SalaryDisbursementDetails detail)
+        {
+            return await _repository.Add(detail);
+        }
+
+        public async Task<SalaryDisbursementDetails?> GetById(int id)
+        {
+            return await _repository.GetById(id);
+        }
+        public async Task<SalaryDisbursementDetails?> Update(SalaryDisbursementDetails details)
+        {
+            return await _repository.Update(details);
+        }
+        public async Task DeleteById(int id)
+        {
+            await _repository.DeleteById(id);
+        }
+    }
+}
