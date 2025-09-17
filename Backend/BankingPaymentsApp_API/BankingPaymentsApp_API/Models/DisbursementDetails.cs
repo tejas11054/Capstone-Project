881cdb4d2c3df00bbc5d4ec8f6a34a256aecd14c
Youbraj Singh <youbrajsingh2576@gmail.com>
2025-09-17 22:05:27 +0530

    Made Salary Disbursement related models

---
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/DisbursementDetails.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingPaymentsApp_API.Models
+{
+    public class DisbursementDetails
+    {
+        [Key]
+        public int DetailId {  get; set; }
+        [ForeignKey("SalaryDisbursement")]
+        public int SalaryDisburementId {  get; set; }
+        public virtual SalaryDisbursement SalaryDisbursement { get; set; }
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+        [ForeignKey("Transaction")]
+        public int? TransactionId { get; set; }
+        public virtual Transaction? Transaction { get; set; }
+    }
+}
---
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Models/SalaryDisbursement.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingPaymentsApp_API.Models
+{
+    public class SalaryDisbursement
+    {
+        [Key]
+        public int SalaryDisbursementId { get; set; }
+        [ForeignKey("ClientUser")]
+        public int ClientId {  get; set; }
+        public virtual ClientUser? ClientUser { get; set; }
+        [Required(ErrorMessage = "Total Amount is Required!")]
+        [DataType(DataType.Currency)]
+        public decimal TotalAmount { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DisbursementDate { get; set; } = DateTime.UtcNow;
+        public virtual ICollection<DisbursementDetails> DisbursementDetailId { get; set; } = new List<DisbursementDetails>();
+
+    }
+}
