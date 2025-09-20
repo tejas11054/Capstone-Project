039764719b476a4c4b8c4e38f069432cf9615006
tejas11054 <tejramane11@gmail.com>
2025-09-20 18:22:05 +0530

    smtp settings added for email

--- Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Services/EmailService.cs
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/Services/EmailService.cs
@@ -1,6 +1,19 @@
-﻿namespace BankingPaymentsApp_API.Services
+﻿using BankingPaymentsApp_API.Repositories;
+
+namespace BankingPaymentsApp_API.Services
 {
-    public class EmailService
+    public class EmailService : IEmailService
     {
+        private readonly IEmailRepository _emailRepository;
+
+        public EmailService(IEmailRepository emailRepository)
+        {
+            _emailRepository = emailRepository;
+        }
+
+        public async Task SendEmailToAuthorAsync(int id)
+        {
+            await _emailRepository.SendEmailToAuthorAsync(id);
+        }
     }
 }
