3e2512c5a08aaae6489be704a0be51c3af7101ef
tejas11054 <tejramane11@gmail.com>
2025-09-23 15:27:54 +0530

    stash

---
+++ Backend/BankingPaymentsApp_API/BankingPaymentsApp_API/DTOs/GoogleCaptchaResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace BankingPaymentsApp_API.DTOs
+{
+    public class GoogleCaptchaResponse
+    {
+        [JsonProperty("success")] 
+        public bool Success { get; set; }
+        [JsonProperty("score")] 
+        public double Score { get; set; } // v3 only
+        [JsonProperty("action")] 
+        public string Action { get; set; }
+        [JsonProperty("challenge_ts")] 
+        public DateTime ChallengeTs { get; set; }
+        [JsonProperty("hostname")] 
+        public string Hostname { get; set; }
+    }
+}
