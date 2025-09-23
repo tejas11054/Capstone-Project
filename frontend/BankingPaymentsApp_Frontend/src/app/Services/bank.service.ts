ced8385cffb29c212919759e5a959ba4589153ca
tejas11054 <tejramane11@gmail.com>
2025-09-23 15:40:40 +0530

    removed log files from tracking

--- frontend/BankingPaymentsApp_Frontend/src/app/Services/bank.service.ts
+++ frontend/BankingPaymentsApp_Frontend/src/app/Services/bank.service.ts
@@ -1,17 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { RegisterBankUserDTO } from '../DTO/RegisterBankUserDTO';
+import { BankUser } from '../Models/BankUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BankRegisterService {
-  private apiUrl = 'https://localhost:7030/api/BankUser';
+  private baseUrl = 'https://localhost:7030/api/BankUser';
 
-  constructor(private http: HttpClient) {}
-
-  registerBankUser(user: RegisterBankUserDTO): Observable<any> {
-    return this.http.post<any>(this.apiUrl, user);
+  constructor(private http: HttpClient) { }
+  
+    registerBankUser(user: BankRegisterService): Observable<any> {
+      return this.http.post<any>(this.baseUrl, user);
+    }
+  
+    getClients(): Observable<BankUser[]> {
+      return this.http.get<BankUser[]>(`${this.baseUrl}/BankUser`);
+    }
+  
+    getBankById(bankId: number): Observable<BankUser> {
+      return this.http.get<BankUser>(`${this.baseUrl}/BankUser/${bankId}`);
+    }
+  
+   approveBank(bankId: number): Observable<any> {
+    return this.http.get<BankUser>(`${this.baseUrl}/BankUser/${bankId}`)
+      .pipe(
+        switchMap((fullClient: BankUser) => 
+          this.http.put(`${this.baseUrl}/BankUser/approve/${bankId}`, fullClient)
+        )
+      );
+  }
+  
+   rejectBank(bankId: number, reason: string): Observable<string> {
+    return this.http.put(`${this.baseUrl}/BankUser/reject/${bankId}`, { reason }, { responseType: 'text' });
   }
 }
